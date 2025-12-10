47c23e40ccb8309aea860743010eb10fe42f184c
Nastexa1 <nastexofeysal@gmail.com>
2025-12-10 09:15:49 -0800

    Restrict CORS to specific origin

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ const express=require("express")
 const mongoose=require("mongoose")
 const cors=require("cors")
 const app=express()
-app.use(cors())
+app.use(cors({
+  origin: "https://fullrestaurant.netlify.app"
+}));
 app.use(express.json())
 
 mongoose.connect("mongodb://localhost:27017/Restaurant").then(()=>{
